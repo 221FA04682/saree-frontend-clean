42f05c1d464c2ba31485787786e6ac3bfdc4b8c0
221FA04682 <elurinareshkumar505@email.com>
2026-04-02 11:36:05 +0530

    fix api url

--- dist/saree-shop/browser/chunk-FUDRQSTN.js
+++
@@ -1,1 +0,0 @@
-import{A as o,I as i,d as n,n as r,v as s}from"./chunk-4KW6S4JT.js";function c(t){t||(o(c),t=s(i));let u=new n(e=>t.onDestroy(e.next.bind(e)));return e=>e.pipe(r(u))}export{c as a};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://saree-backend-mtlb.onrender.com/api',
+  production: true,
+  apiUrl: 'https://saree-backend-mtlb.onrender.com'
 };
